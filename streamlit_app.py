f1eff2604d6979befe593bc2fcd0c4138f9d194e
Sergio Lucero <sergiolucero@gmail.com>
2023-08-18 07:33:04 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,6 @@ st.write(f'Hable con ella')
 #os.environ['REPLICATE_API_TOKEN'] = "REPLICATE API TOKEN"
 #bot.add("https://www.youtube.com/watch?v=Hpb9lmQxguo&t=306s")
 #zuck_bot.add("https://en.wikipedia.org/wiki/Mark_Zuckerberg")
-bot.query("Qué pasa con el barrio puerto?")
 st.write("Qué pasa con el barrio puerto?")
 txt="El barrio Puerto ha experimentado despoblamiento y deterioro. Según un estudio realizado por Pablo Trielli, actualmente viven alrededor de 870 personas en el barrio Puerto, lo cual es muy poco en comparación con los aproximadamente 12.000 habitantes de toda la zona típica. La foto muestra un pasaje del barrio Puerto en un día de semana."
 st.info(txt)
