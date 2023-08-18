fe07dd3a39ec8181061ab3de0be7f523d7ed377e
Sergio Lucero <sergiolucero@gmail.com>
2023-08-18 08:31:23 -0400

    added new video

--- chatlib.py
+++ chatlib.py
@@ -10,4 +10,5 @@ os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
 #zuck_bot.add("https://en.wikipedia.org/wiki/Mark_Zuckerberg")
 
 bot = App()
+bot.add("https://www.youtube.com/watch?v=MWP-LjOOuQ4")
 bot.add('web_page', 'https://raw.githubusercontent.com/sergiolucero/maria/master/youmaria.txt')
