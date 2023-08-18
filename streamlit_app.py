40736eb168b03b94f0f78f0b196928f406f2c542
Sergio Lucero <sergiolucero@gmail.com>
2023-08-18 07:32:30 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import os
-#from embedchain import App
+from embedchain import App
 import streamlit as st
 #from textlib import COMPLETION_MODEL, TRANSCRIPTION_MODEL, VERSION
 #from linelib import simple_recorder # was linetabs
@@ -12,10 +12,9 @@ st.write(f'Hable con ella')
 #simple_recorder()
 #from embedchain import Llama2App
 #os.environ['REPLICATE_API_TOKEN'] = "REPLICATE API TOKEN"
-#bot = App()
 #bot.add("https://www.youtube.com/watch?v=Hpb9lmQxguo&t=306s")
 #zuck_bot.add("https://en.wikipedia.org/wiki/Mark_Zuckerberg")
-#bot.query("Qué pasa con el barrio puerto?")
+bot.query("Qué pasa con el barrio puerto?")
 st.write("Qué pasa con el barrio puerto?")
 txt="El barrio Puerto ha experimentado despoblamiento y deterioro. Según un estudio realizado por Pablo Trielli, actualmente viven alrededor de 870 personas en el barrio Puerto, lo cual es muy poco en comparación con los aproximadamente 12.000 habitantes de toda la zona típica. La foto muestra un pasaje del barrio Puerto en un día de semana."
 st.info(txt)
@@ -24,4 +23,8 @@ st.write("cuáles son las problemáticas?")
 txt="Las problemáticas mencionadas son el deterioro de viviendas, incendios, plagas como ratones y termitas, y la población canina descontrolada."
 st.info(txt)
 
-st.header('Fuente: https://www.youtube.com/watch?v=Hpb9lmQxguo              ')
+st.header('Fuente: https://www.youtube.com/watch?v=Hpb9lmQxguo')
+
+bot = App()
+bot.add('web_page', 'https://raw.githubusercontent.com/sergiolucero/maria/master/youmaria.txt')
+bot.query('quiénes participaron de la conversación?')
