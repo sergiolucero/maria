5086cfee20dbfe05ae32576689d6eb243ccf9dfe
Sergio Lucero <sergiolucero@gmail.com>
2023-08-18 03:55:30 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,12 @@ st.set_page_config(layout="wide")
 st.title('👩‍🎤Ella es la María🤖')
 st.write(f'Hable con ella')
 ############################
-simple_recorder()
+#simple_recorder()
+import os
+from embedchain import Llama2App
+
+os.environ['REPLICATE_API_TOKEN'] = "REPLICATE API TOKEN"
+zuck_bot = Llama2App()
+zuck_bot.add("https://www.youtube.com/watch?v=Hpb9lmQxguo&t=306s")
+#zuck_bot.add("https://en.wikipedia.org/wiki/Mark_Zuckerberg")
+zuck_bot.query("Qué pasa con el barrio puerto?")
