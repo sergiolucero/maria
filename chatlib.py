594d11e692711491044c093734bf3534532fb32a
Sergio Lucero <sergiolucero@gmail.com>
2023-08-18 07:44:17 -0400

    Create chatlib.py

---
+++ chatlib.py
@@ -0,0 +1,12 @@
+import os, openai
+from embedchain import App
+
+os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
+
+#from embedchain import Llama2App
+#os.environ['REPLICATE_API_TOKEN'] = "REPLICATE API TOKEN"
+#bot.add("https://www.youtube.com/watch?v=Hpb9lmQxguo&t=306s")
+#zuck_bot.add("https://en.wikipedia.org/wiki/Mark_Zuckerberg")
+
+bot = App()
+bot.add('web_page', 'https://raw.githubusercontent.com/sergiolucero/maria/master/youmaria.txt')
