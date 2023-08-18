6d15773fd48eb61f195fc0392820db3850af6cf7
Sergio Lucero <sergiolucero@gmail.com>
2023-08-18 04:13:56 -0400

    commenting out

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,16 @@
+import os
+from embedchain import App
 import streamlit as st
-from textlib import COMPLETION_MODEL, TRANSCRIPTION_MODEL, VERSION
-from linelib import simple_recorder # was linetabs
-from datetime import datetime
+#from textlib import COMPLETION_MODEL, TRANSCRIPTION_MODEL, VERSION
+#from linelib import simple_recorder # was linetabs
+#from datetime import datetime
 ##############################################
 st.set_page_config(layout="wide")
 st.title('👩‍🎤Ella es la María🤖')
 st.write(f'Hable con ella')
 ############################
 #simple_recorder()
-import os
 #from embedchain import Llama2App
-from embedchain import App
 #os.environ['REPLICATE_API_TOKEN'] = "REPLICATE API TOKEN"
 bot = App()
 bot.add("https://www.youtube.com/watch?v=Hpb9lmQxguo&t=306s")
