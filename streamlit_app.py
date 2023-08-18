1f19eb9ef25d15473e6eb2731ec881087e133959
Sergio Lucero <sergiolucero@gmail.com>
2023-08-18 07:34:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.set_page_config(layout="wide")
 st.title('👩‍🎤Ella es la María🤖')
 st.write(f'Hable con ella')
 ############################
-os.environ['OPENAI_API_KEY'] = st.secrets('OPENAI_API_KEY')
+os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
 #simple_recorder()
 #from embedchain import Llama2App
 #os.environ['REPLICATE_API_TOKEN'] = "REPLICATE API TOKEN"
